33eedd2b3b112f6714bafc4acd46855c98c92030
Mogakamo <mogaka.amo254@gmail.com>
2022-11-13 08:06:54 +0300

    [UI]: finished with the player

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import router from './navigation'
+import { MantineProvider } from "@mantine/core";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import router from "./navigation";
+import './styles/index.css'
 
-const route = createBrowserRouter([
-  {}
-])
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const route = createBrowserRouter([{}]);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider  router={router} />
+    <MantineProvider theme={{
+        fontFamily: "Open Sans, sans-serif",
+        lineHeight: 1.2,
+        primaryColor: "indigo",
+      }}
+    >
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>
-)
+);
--- src/routes/landing/index.tsx
+++ src/routes/landing/index.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import {
+  Center,
+  Col,
+  Container,
+  Flex,
+  Grid,
+  Progress,
+  Text,
+} from "@mantine/core";
+import React from "react";
+import * as styles from "./index.modules.css";
+import {
+  IconPlayerPlay,
+  IconPlayerTrackNext,
+  IconPlayerTrackPrev,
+  IconPlayerPause,
+  IconPlayerSkipForward,
+  IconPlayerSkipBack,
+  IconVolume
+} from "@tabler/icons";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container
+      size="xl"
+      style={{ height: "100vh", backgroundColor: "#191970" }}
+      fluid
+    >
+      <Center style={{ width: "100%", height: "100%" }}>
+        <Flex direction="column" style={{ width: "100%" }}>
+          <Progress value={50} color="orange"  style={{ width: "100%" }} />
 
-export default Home
+          <Grid style={{ marginTop: 10 }}>
+            <Col span={5}>
+              <Text weight={700} size="xl" style={{ color: "white" }}>
+                MUD
+              </Text>
+              <Text>by Flumes</Text>
+            </Col>
+            <Col span={3}>
+              <Flex align="center" gap="xl" style={{cursor: 'pointer'}}>
+                {/* <IconPlayerSkipBack /> */}
+                <IconPlayerTrackPrev color="white" stroke={2} size={26} />
+                <IconPlayerPlay color="white" stroke={2} size={36} />
+                <IconPlayerTrackNext color="white" stroke={2} size={26} />
+              </Flex>
+            </Col>
+            <Col span={4}> 
+              <Flex align="flex-end" justify="flex-end" style={{marginRight: 40}}>
+              <IconVolume color="white" stroke={2} size={26} />
+              </Flex>
+            </Col>
+          </Grid>
+        </Flex>
+      </Center>
+    </Container>
+  );
+};
+
+export default Home;
